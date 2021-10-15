96ae10043f5713433c530c391b8dd74117ab14b6
antonio123 <artemiokaoz@gmail.com>
2021-10-15 11:16:44 -0500

    Solucionando error del formato de la fecha

--- generar.php
+++ generar.php
@@ -9,7 +9,7 @@ if(isset($_POST))
    $nombre .= isset($_POST['apellidoP']) ? ' ' . ucwords(strtolower($_POST['apellidoP'])) : 'NULO';
    $nombre .= isset($_POST['apellidoM']) ? ' ' . ucwords(strtolower($_POST['apellidoM'])) : 'NULO';
    $fecha = isset($_POST['fechaA']) ?  $_POST['fechaA'] : 'NULO';
-   $tiempo = date('m/d/Y h:i:s a' , time());
+   $tiempo = date('d/m/Y h:i:s a' , time());
    $documento = strtoupper($_POST['nombre'][0]).strtoupper($_POST['apellidoP'][0]).strtoupper($_POST['apellidoM'][0]).rand(1,getrandmax());
    $fecha = date_create($fecha);
    $fecha = date_format($fecha,'d/m/Y');
